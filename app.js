801e4822cf8ba023e0496775b604c66ad079c6c2
MDC 219 <mcouch219@gmail.com>
2022-04-08 17:55:38 -0400

    DomBasicsExercise

---
+++ app.js
@@ -0,0 +1,73 @@
+const { getegid, prependListener } = require("process");
+
+//dom basicse
+const xMen = document.getElementById(`x-men`);
+const centered = document.getElementClassName(`center`);
+const batMan = document.querySelector(`#batman`);
+
+const h3s = document.querySelector(`h3s`);
+
+const h1 = document.querySelector(`h1`);
+
+h1.innerText = `The Best Animated TV Show Ever!`;
+
+const honorableMentions = document.querySelector(`honorable-mentions`);
+
+honorableMentions.innerHtml = `<p>OTHER GREAT SHOWS</p>`;
+
+aTag.href = "https://fandomwire.com/15-greatest-animated-superhero-shows-ever-made-ranked/";
+
+
+h1.classList.add("background , text-color");
+
+h1.classList.remove("background");
+
+
+const h4 = document.createElement("h4");
+
+h4.innerText = " Is Avatar";
+
+const body =document.querySelector(`body`);
+prependListener(h4);
+
+const h5 = document.createElement("h5");
+
+h5.innerText = "Heroes in a Half Shell Turtle Power!";
+
+a.insertadjacentElement("afterend", h5);
+
+const li = document.querySelector("ul>li");
+li.remove();
+
+
+const divs = document.querySelectorAll(`div`);
+divs.forEach((div) =>
+div.classList.toggle(`background`)
+);
+
+const avengers =[
+
+    "forever",
+    "fight",
+    "As",
+    "ONE",
+    "AVENGERS",
+    "ASSEMBLE!"
+];
+const newRay = document.createElement(`span`);
+function adding (a){
+    for(i=0; i<a.length; i++){
+        newRay.innerText += a[i]+ ` `;
+    }
+    body.append(newRay);
+}
+
+adding(avengers);
+
+
+
+
+
+
+
+
